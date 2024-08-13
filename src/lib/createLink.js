6ed069d25a8f95b80a36ad656338c3295ead2ea1
barab <94920370+Baraban511@users.noreply.github.com>
2024-08-13 18:35:25 +0200

    Change host

--- src/lib/createLink.js
+++ src/lib/createLink.js
@@ -3,7 +3,7 @@ export default function createLink(link) {
     let cn = encodeURIComponent(link.cn);
     var password = encodeURIComponent(link.password.data);
     var iv = encodeURIComponent(link.password.iv);
-    var host = 'taquin.tech';
+    var host = 'taquin-4i2.pages.dev';
     var id = encodeURIComponent(link.login);
     if (import.meta.env.DEV) {
         host = link.host;
