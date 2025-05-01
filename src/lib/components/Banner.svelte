211e62ad3b775f5772aa679abe4c18c8a6956dc8
barab. <Baraban511@users.noreply.github.com>
2025-05-01 10:59:57 +0200

    Minor fixes

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="flex justify-center items-center relative top-0 z-50 w-full h-10 bg-red-500"
+  class="flex justify-center items-center relative top-0 z-50 w-full min-h-10 bg-red-500 p-1"
 >
-  {env.PUBLIC_BANNER_TEXT}
+  <p class="text-center">{env.PUBLIC_BANNER_TEXT}</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
+  import { fade } from "svelte/transition";
   import { fly } from "svelte/transition";
-  import { tweened } from "svelte/motion";
-  import { flip } from "svelte/animate";
   import { env } from "$env/dynamic/public";
   import Footer from "$lib/components/Footer.svelte";
   import Banner from "$lib/components/Banner.svelte";
@@ -14,9 +13,10 @@
   import ConnexionBox from "$lib/components/ConnexionBox.svelte";
   import StatusBox from "$lib/components/StatusBox.svelte";
   import TablerX from "~icons/tabler/x";
-  let { data, form = $bindable() } = $props();
-  let Components = $derived([{ component: ConnexionBox, props: { data } }]);
-  $effect(() => {
+  let { data, form } = $props();
+  let popup = $state(true);
+  let Components = $derived([]);
+  $effect.pre(() => {
     if (data?.status === "offline") {
       Components = [
         { component: Header },
@@ -25,12 +25,23 @@
         { component: Functionalities },
         { component: Faq },
       ];
-    } else if (data?.step === "QCM") {
+    }
+    if (data?.step === "QCM") {
       Components = [{ component: ConnexionBox, props: { data } }];
-    } else if (data?.step === "LINK") {
+    }
+    if (data?.step === "LINK") {
       Components = [
         { component: ConnexionBox, props: { data } },
         { component: Contact, props: { mail: data?.mail } },
+      ];
+    }
+    if (!data.step) {
+      Components = [
+        { component: Header },
+        { component: ConnexionBox, props: { data } },
+        { component: HowItWorks },
+        { component: Functionalities },
+        { component: Faq },
       ];
     }
   });
@@ -41,13 +52,12 @@
   if (data?.step === "LINK") {
     pageTitle = "Exportez votre calendrier";
   }
-  console.log(env.PUBLIC_BANNER);
 </script>
 
 <svelte:head>
   <!-- Primary Meta Tags -->
   <title>Taquin - {pageTitle}</title>
-  <link rel="canonical" href="https://taquin.tech/" />
+  <link rel="canonical" href="https://taquin.barab.me/" />
 </svelte:head>
 
 <noscript>
@@ -66,18 +76,18 @@
 >
   <Progress step={data.step} />
   {#each Components as { component: Component, props = { } }, key (Component)}
-    <div animate:flip>
+    <div animate:fade>
       <Component {...props} />
     </div>
   {/each}
 </div>
 <!--Error pop-up-->
-{#if form?.error}
+{#if form?.error && popup}
   <div
     transition:fly={{ x: -200, duration: 700 }}
     role="alert"
     id="alert"
-    class="z-50 rounded-xl border border-gray-100 bg-white p-4 dark:border-gray-800 dark:bg-gray-900 fixed bottom-2 left-2"
+    class="z-40 rounded-xl border border-gray-100 bg-white p-4 dark:border-gray-800 dark:bg-gray-900 fixed bottom-2 left-2"
   >
     <div class="flex items-start gap-4">
       <div class="flex-1">
@@ -91,7 +101,7 @@
       </div>
 
       <button
-        onclick={() => (form.error = false)}
+        onclick={() => (popup = false)}
         class="text-gray-500 transition hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-500"
       >
         <span class="sr-only">Fermer la notification</span>
